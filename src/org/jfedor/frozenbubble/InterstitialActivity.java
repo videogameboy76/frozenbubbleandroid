803ce080652fcf9f17847009a461dffba2c7aaf8
forteri76 <videogameboy76@users.noreply.github.com>
2013-07-12 02:27:02 +0000

    Fixed bug that can arise when no view activity is found for whatever reason.

--- src/org/jfedor/frozenbubble/InterstitialActivity.java
+++ src/org/jfedor/frozenbubble/InterstitialActivity.java
@@ -1,15 +1,16 @@
 package org.jfedor.frozenbubble;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class InterstitialActivity extends Activity {
   Button gotoPlayStoreButton;
@@ -35,10 +36,21 @@ public class InterstitialActivity extends Activity {
     OnClickListener gotoPlayStore = new OnClickListener() {
       @Override
       public void onClick(View v) {
-        String url = "market://details?id=org.jfedor.beaver&referrer=utm_source%3Dfrozenbubble";
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setData(Uri.parse(url));
-        startActivity(i);
+        Intent i = new Intent(Intent.ACTION_VIEW,
+            Uri.parse("market://details?id=org.jfedor.beaver&referrer=utm_source%3Dfrozenbubble"));
+        try {
+          startActivity(i);
+        } catch (ActivityNotFoundException anfe1) {
+          i = new Intent(Intent.ACTION_VIEW,
+              Uri.parse("market://search?q=busy beaver"));
+          try {
+            startActivity(i);
+          } catch (ActivityNotFoundException anfe3) {
+            Toast.makeText(getApplicationContext(), 
+                "Could not access market.  Are you connected to the internet?",
+                Toast.LENGTH_SHORT).show();
+          }
+        }
       }
     };
 
